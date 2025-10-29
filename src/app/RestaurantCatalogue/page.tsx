d5ebecd2a356093a7b20b4f7145ac98a1370c056
thadoos <thaddeus.ong.23@ucl.ac.uk>
2025-10-29 17:13:55 +0000

    Added buttons for login and going to manager dashboard in navbar

---
+++ src/app/RestaurantCatalogue/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function RestaurantCatalogue(){
+  return (
+    <div className="flex w-full">
+
+    </div>
+  )
+}
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,27 +6,22 @@ import Link from "next/link";
 
 import styles from "./navbar.module.css";
 
-// function NavbarAction({ onLinkClick }: { onLinkClick?: () => void }) {
-//   return (
-//     <div className="flex gap-4 items-center max-sm:flex-col-reverse max-sm:mt-8 text-black">
-//       <Link href={"/event-order"} onClick={onLinkClick}>
-//         <button className="btn btn-md btn-ghost rounded-full text-primary  hover:bg-primary border-0 hover:text-white text-lg">
-//           EVENT ORDERING
-//         </button>
-//       </Link>
-//       <Link href={"/#aboutus"} onClick={onLinkClick}>
-//         <button className="btn btn-md btn-ghost rounded-full text-primary  hover:bg-primary border-0 hover:text-white text-lg">
-//           ABOUT
-//         </button>
-//       </Link>
-//       <Link href={"/contact"} onClick={onLinkClick}>
-//         <button className="btn btn-md btn-ghost rounded-full text-primary hover:bg-primary border-0 hover:text-white text-lg">
-//           CONTACT US
-//         </button>
-//       </Link>
-//     </div>
-//   );
-// }
+function NavbarAction({ onLinkClick }: { onLinkClick?: () => void }) {
+  return (
+    <div className="flex gap-4 items-center max-sm:flex-col-reverse max-sm:mt-8 text-black">
+      <Link href={"/event-order"} onClick={onLinkClick}>
+        <button className="btn btn-md bg-white hover:bg-gray-50 rounded-md text-black border-1 border-black font-semibold text-base px-6">
+          MANAGER
+        </button>
+      </Link>
+      <Link href={"/#aboutus"} onClick={onLinkClick}>
+        <button className="btn btn-md bg-primary hover:bg-primary/90 rounded-md text-white border-0 font-semibold text-base px-6">
+          LOGIN
+        </button>
+      </Link>
+    </div>
+  );
+}
 
 export default function Navbar() {
   const closeDrawer = () => {
@@ -40,7 +35,10 @@ export default function Navbar() {
 
   return (
     <nav className="sticky top-0 left-0 right-0 flex flex-col z-50">
-      <div className="flex items-center justify-center px-16 py-4 bg-secondary gap-5 flex-nowrap">
+      <div className="flex items-center justify-between px-16 py-4 bg-secondary gap-5 flex-nowrap">
+        <div className="invisible max-xl:hidden whitespace-nowrap">
+          <NavbarAction />
+        </div>
         <Link href={"/"} className="cursor-pointer">
           <div className="flex items-center gap-4 cursor-pointer h-full whitespace-nowrap group relative">
             <div className="relative">
@@ -60,6 +58,31 @@ export default function Navbar() {
             </div>
           </div>
         </Link>
+        <div className="visible max-md:hidden whitespace-nowrap">
+          <NavbarAction />
+        </div>
+        <div className="drawer w-fit hidden max-md:block">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            <label
+              className="btn btn-ghost btn-square drawer-button hover:bg-primary/10 border-0"
+              htmlFor="my-drawer"
+            >
+              <Menu size={28} color="var(--color-primary)" />
+            </label>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <div className="h-full bg-white w-[80%]">
+              <div className="px-3 mt-4">{/* <SearchBar /> */}</div>
+              <NavbarAction onLinkClick={closeDrawer} />
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
